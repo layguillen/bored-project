887c1b9b9b7d0862a3581222b896194ac09d6528
Layla Guillen <laylazahniser@gmail.com>
2023-03-06 17:09:00 -0600

    added application properties and api url

--- Bored-project-backend/src/main/java/com/example/Boredproject/services/BoredService.java
+++ Bored-project-backend/src/main/java/com/example/Boredproject/services/BoredService.java
@@ -1,7 +1,18 @@
 package com.example.Boredproject.services;
 
+import com.example.Boredproject.models.Activity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
 public class BoredService {
+    //@Value annotation allows us to grab the values from application.properties
+    @Value("${BORED_API}")
+    private String BORED_API;
 
+    //RestTemplate is how we communicate with API
+    RestTemplate restTemplate = new RestTemplate();
 
-
+    public Activity getActivity(){
+        return null;
+    }
 }
