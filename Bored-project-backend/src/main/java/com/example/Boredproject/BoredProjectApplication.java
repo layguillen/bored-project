0e84bb96b098a8cca34bf3ef710f2ed5da185aaf
Layla Guillen <laylazahniser@gmail.com>
2023-03-06 16:35:03 -0600

    backend started

---
+++ Bored-project-backend/src/main/java/com/example/Boredproject/BoredProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Boredproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoredProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoredProjectApplication.class, args);
+	}
+
+}
---
+++ Bored-project-backend/src/test/java/com/example/Boredproject/BoredProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Boredproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoredProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
